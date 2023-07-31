917ca3142f72968a3236a17ab7dfe258ee1e6ec9
root <root@DESKTOP-6I3RBRC>
2023-07-31 23:08:29 +0300

    Adding JavaScript functions

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-function display(val){
-
-    document.getElementById('result').value += val
-
-    return val
-
+function display(value) {
+    document.getElementById("result").value += value;
 }
+
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+
+function solve() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
+
+
+// let calculate= document.getElementsByClassName('operation')
+// calculate.addEventListener('click', (e) =>{
+//     document.getElementById('result').value += e.val
+// })
